9be686fdd4064ab39460dcf07e83d5a2903eb903
Piyush <gaurpiyush001@gmail.com>
2022-01-21 01:21:26 +0530

    with_models_and_auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,106 @@
+//Here we'll do all User Related Stuff/////////////////////////////////
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+  return jwt.sign(
+    { id } /*Payload*/,
+    process.env.JWT_SECRET /*secret-Key_32-char-long*/,
+    /*Options*/ {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  );
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  }); //Creating a New Document Based on the Models
+  // By sending this selected data in signUp, we prevented the possiblity of manipulating the admin role
 
+  // ------------------------IMPLEMENTING AUTHENTICATION-----------------------------//
+
+  // 1.) Craeting a unique JWT, during signup
+  const token = jwt.sign(
+    { id: newUser._id } /*Payload*/,
+    process.env.JWT_SECRET /*secret-Key_32-char-long*/,
+    /*Options*/ {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  );
+  // token headers are craeted automatically
   res.status(201).json({
+    //We will send the created user to client
     status: 'success',
+    token,
     data: {
       user: newUser
     }
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1.) Check or Verify requested Email and Password exist or not in user request body data
+  if (!email || !password) {
+    return next(
+      new AppError('Please provide email and password!', 400 /*Bad Request*/)
+    );
+  }
+
+  // 2.) check if user exist && password is correct
+  const user = await User.findOne({ email }).select(
+    '+password'
+  ); /*explicitly slecting password*/
+
+  //Comparing decrypted password of database with that given by the user in request(Done in Modal)
+  // if(user.password === password)
+  //const correct = await user.correctPassword(password, user.password);
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3.) If everything ok, send unique token to client
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting Token from user and check if its there in request body
+  //Common practice is to send a token using an http header with the request
+  //because const and let are blockscope
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError(
+        `You are npt logged in! Please log in to get access.`,
+        401 /*unauthorized*/
+      )
+    );
+  }
+  // 2) Verification token
+
+  // 3) If User still exist
+
+  // 4) Check If user changed password after teh JWT was issued
+
+  next();
 });
---
+++ models/userModel.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+// ------------FAT MODELS , THIN CONTROLLERS --------------//
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    //this is a unique identifier of each User
+    type: String,
+    required: [true, 'Please tell us your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide Password'],
+    minlength: 8,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please Confirm your Password'],
+    validate: {
+      // We will create a function and error message, below is a custom validator, which will only work on saving the document
+      // below validation will not work on updating a document
+      validator: function(el) {
+        return el === this.password; // Equating the Password to check
+      },
+      message: 'Passwords are not the same' // error message
+    }
+  }
+});
+
+// For doing Password Encryption Password, we will use pre save hook(), i.e document middleware
+// manipulating data between getting the data and saving it to the database
+userSchema.pre('save', async function(next) {
+  //we will only encrypt the password, when user has updated/created the Password field
+  if (!this.isModified('password')) return next(); //All documents have a method --> isModified
+
+  // Hashing/Encrypting the password, we will do encryption using an algorithm called Bcrypt, first
+  // this algo will salt the password and then encrypt/hash
+  this.password = await bcrypt.hash(
+    this.password,
+    10 /*Cost parameter for salting*/
+  );
+  this.passwordConfirm = undefined; //we don't want to persist confirm password to database
+  next();
+});
+
+///////////---------------------------INSTANCE METHOD, for decrypting the password for verificaation at time of logginIn------------------/////////////////
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  //this.password is not available in the output, so we pass candidatePassword as a parameter in function
+  //just returns true or false
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema); //creating model out of schema
+
+module.exports = User;
