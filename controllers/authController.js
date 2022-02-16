85ebb394d0d445b5cf6a360560855b0174fbff41
Piyush Gaur <gaurpiyush001@gmail.com>
2022-02-16 23:16:38 +0530

    Finalauthentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,11 @@
 //Here we'll do all User Related Stuff/////////////////////////////////
+const crypto = require('crypto');
 const { promisify } = require('util'); //util module use for promisifying a function
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const sendEmail = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign(
@@ -153,17 +155,127 @@ exports.restrictTo = (...roles) => {
   };
 };
 
+// Basic Idea -> We just need to provide out emailAddress and we will then get an email ,
+//in that we will be having a link on clicking that its gonna take us to page, where we can put a new Password
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   console.log('In forgot Password controller');
   //1.) Get user based on POSTED email
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return next(new AppError('there is no user with email address', 404));
+    return next(new AppError('There is no user with email address', 404));
   }
-  //2.) Generate the random token(will do in Instance Methods in model)
+  //2.) Generate the random token not a Json web Token(will do in Instance Methods in model)
   const resetToken = user.createPasswordResetToken();
-  console.log(resetToken);
-  await user.save({ validateBeforeSave: false });
+  console.log(resetToken, 'In authController after executing InstanceMethod');
+  await user.save(
+    {
+      validateBeforeSave: false
+    } /*this option will deactivaet all the validators that we set in Our Schema*/
+  ); //saving the modified Document
+
   //3.) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did'nt forget your password, please ignore this e-mail`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your PASSWORD RESET TOKEN (valid for 10min)',
+      message
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email '
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validationBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try Again later!'),
+      500 /*server error*/
+    );
+  }
 });
-exports.resetPassword = (req, res, next) => {};
+
+//Password Reset Functionality
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  //1.) Get user based on the token
+  //now we will encrypt the randomToken to match with to the encrypted resetToken present in database
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  //2.) If token has NOT EXPIRED, and there is user exist, THEN ONLY SET NEW PASSWORD
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  //3.) Update changedPasswordAt property for the current user
+  //This is done by pre middleware in user model
+
+  //4.) Log the user In, send JWT
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token
+  });
+});
+
+//By Below controller we allow LoggedIn user to Update his password without forgetting it(without that whole reset process)
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  //This password updating functionality is only for logged in user, but still we need to pass in his current password, So in order to confirm that the user actually is who he claims to be(Just for Security measure)
+  // 1) Get user from collection
+  console.log('My request body', req.body);
+  const user = await User.findById(req.user.id).select('+password');
+  // const user = await User.findOne({ email: req.body.email }).select(
+  //   '+password'
+  // );
+  console.log(user, 'In updatePassword');
+
+  // 2) Check if Posted curent password is correct
+  const previousPassword = req.body.prevPassword;
+  console.log(req.body.prevPassword);
+  if (!user || !(await user.correctPassword(previousPassword, user.password))) {
+    // console.log('In first block');
+    return next(
+      new AppError('Your current Password is wrong', 401 /*Unauthorized*/)
+    );
+  }
+  if (req.body.newPassword !== req.body.newPasswordConfirm) {
+    console.log('In second block');
+    return next(new AppError('Updated ConfirmPassword should be same', 404));
+  }
+
+  // 3) If so, Update password
+  // console.log('Hiiii', req.body.newPassword, req.body.newPasswordConfirm);
+  user.password = req.body.newPassword;
+  user.passwordConfirm = req.body.newPasswordConfirm;
+  await user.save();
+
+  // 4) Log user In, send JWT(with thw new password that was updated)
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token
+  });
+});
