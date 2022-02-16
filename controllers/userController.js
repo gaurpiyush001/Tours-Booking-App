ee265a1a6d93b71205f14ac85473978b280fee7c
Piyush Gaur <gaurpiyush001@gmail.com>
2022-02-16 23:15:43 +0530

    userFeatures

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -13,6 +15,20 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+//logged in user can update his data by thus controller
+exports.updateMe = (req, res, next) => {
+
+  // 1) Create error if user POSTs password data
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates, Please use /updatePassword', 400/*bad request*/));
+  }
+
+  // 2) Update user document
+  res.status(200).json({
+    status: 'success'
+  });
+}
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,8 +8,16 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword
+);
+
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router
   .route('/')
