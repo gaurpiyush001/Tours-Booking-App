46b048fce0d4f7b047143f7e981733e744018bd6
Piyush Gaur <gaurpiyush001@gmail.com>
2022-03-02 21:30:51 +0530

    view

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
+const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get all tour data from the collection
@@ -28,6 +29,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
   // console.log(tourData);
   //  console.log({slug});
+
+  if (!tourData) {
+    return next(
+      new AppError('There is no tour with that name.', 404 /*NotFound*/)
+    );
+  }
+
   res.status(200).render('tour', {
     title: `${tourData.name} Tour`,
     tourInfo: tourData
@@ -35,13 +43,36 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getloginForm = catchAsync(async (req, res, next) => {
-  res
-    .status(200)
-    .set(
-      'Content-Security-Policy',
-      "connect-src 'self' https://cdnjs.cloudflare.com"
-    )
-    .render('login', {
-      title: `Login`
-    });
+  res.status(200).render('login', {
+    title: `Login`
+  });
+});
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your Account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  //console.log('UPDATING USER', req.body);//this will be empty, because we need to add a middleware to get access of data from a submitted form
+
+  const { name, email } = req.body;
+  console.log(req.user, 'This from controller');
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name,
+      email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+  console.log(updatedUser, 'This from controller');
+  res.status(200).render('account', {
+    title: 'Your Account',
+    user: updatedUser //we need to specify here new updatedUser
+  });
 });
